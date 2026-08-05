034fd1324b210b52f2745afe3d6ccbb057ea7d74
Dev Letwala <letwala.dev@gmail.com>
2026-08-05 22:30:29 +0530

    .bat file change in launcher

--- dist/Launch AURA SIMULATOR.bat
+++ dist/Launch AURA SIMULATOR.bat
@@ -1,7 +1,7 @@
 @echo off
 title AURA SIMULATOR
 java -cp "%~dp0AuraSimulator.jar;%~dp0..\lib\*" ^
-     --module-path "C:\Program Files\Java\javafx-sdk-17.0.19\lib" ^
+     --module-path "C:\Program Files\Java\javafx-sdk-17.0.18\lib" ^
      --add-modules javafx.controls,javafx.media,javafx.swing,javafx.graphics ^
      SplashScreen
 
